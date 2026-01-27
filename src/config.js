d6f33c7409539476af64fbb194570e4f22e0fb3c
amedina <medinaalberth3@gmail.com>
2026-01-27 09:37:28 -0400

    agregando url backend

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: import.meta.env.VITE_API_URL || 'https://alberth217-quiniela-2026-ree1.vercel.app'
+    API_URL: import.meta.env.VITE_API_URL || 'alberth217-quiniela-2026-m5gd.vercel.app'
 };
 
 export default config;
